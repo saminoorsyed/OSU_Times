0476d86074ff38979ad0fae32df1efaf47ee6403
Jon Riemer <86006195+Seth-9999@users.noreply.github.com>
2023-03-05 08:49:05 -0500

    Update commentsModel.mjs

--- api/models/commentsModel.mjs
+++ api/models/commentsModel.mjs
@@ -4,9 +4,9 @@ import { pool } from './dbConnector.mjs';
 // returns promise
 // interior data is json (array of user objects)
 
-export async function getCommentsList() {
+export async function getCommentIDList() {
     const [result] = await pool.query(
-        `Select commend_id, comment_text
+        `Select comment_id, comment_text
         From Comments2`);
     return result;
 }
@@ -17,20 +17,23 @@ export async function getCommentsList() {
 
 export async function getComments() {
     const [result] = await pool.query(`
-    Select comment_id, post_id, user_id, comment_text, date_commented
-    from Comments2`);
+    Select Comments2.comment_id, Comments2.post_id, Posts2.title, 
+        Comments2.user_id, Users2.full_name as 'Users Full Name', comment_text, date_commented
+    from Comments2
+    left join Posts2 on Comments2.post_id = Posts2.post_id
+    left join Users2 on Comments2.user_id = Users2.user_id;`);
     return result;
 }
 
 // no inputs accepted
 // returns promise
 // interior data is json (array of user objects)
-export async function GetCommentsColumns(){
+export async function getCommentColumns(){
     const [result] = await pool.query(`
                 SELECT * 
                 FROM INFORMATION_SCHEMA.COLUMNS 
                 WHERE TABLE_NAME = N'Comments2';`)
-    return result;
+    return result.map(({COLUMN_NAME}) => COLUMN_NAME);
 }
 
 // inputs:  (int/str, string, string, string, string)
@@ -39,28 +42,31 @@ export async function GetCommentsColumns(){
 //          {numUsersUpdated: 1, status: updated user}
 //          {numUsersUpdated: 0, status: failed to update user}
 
-export async function updateUser(comment_id, post_id, user_id, comment_text, date_commented) {
+export async function updateComment(comment_id, post_id, user_id, comment_text) {
     let numberRecordsUpdated = 0
+    const CODE_UNIQUE_CONSTRAINT_FAILED = 1062;
     let result_set_header;
     try {
         result_set_header = await pool.query(`
             update Comments2
-            set post_id = ?, user_id = ?, comment_text = ?, date_commented = ?
+            set post_id = ?, user_id = ?, comment_text = ?
             where comment_id = ?`,
-            [post_id, user_id, comment_text, date_commented, comment_id],
+            [post_id, user_id, comment_text, comment_id],
         )
     } catch (error) {
         if (error.errno === CODE_UNIQUE_CONSTRAINT_FAILED) {
-            return { numUsersUpdated: 0, status: "null comment id# issue" };
+            return { numUpdated: 0, status: "null comment id# issue" };
         }
         return error
     }
 
+
     numberRecordsUpdated = result_set_header[0].affectedRows;
+    console.log(numberRecordsUpdated);
     if (numberRecordsUpdated === 1) {
-        return { numUsersUpdated: numberRecordsUpdated, status: "updated comment" };
+        return { numUpdated: numberRecordsUpdated, status: "updated comment" };
     } else {
-        return { numUsersUpdated: numberRecordsUpdated, status: "failed to update comment" };
+        return { numUpdated: numberRecordsUpdated, status: "failed to update comment" };
     }
 }
 
@@ -95,15 +101,15 @@ export async function deleteComment(comment_id) {
 //    {numUsersAdded: 0, status: not unique user}
 // otherwise throws error
 
-export async function addComment(post_id, user_id, comment_text, date_commented) {
+export async function addComment(post_id, user_id, comment_text) {
     const CODE_UNIQUE_CONSTRAINT_FAILED = 1062;
     let numberUserAdded;
     let result_set_header;
 
     try {
         result_set_header = await pool.query(`
-                insert into Users(post_id, user_id, comment_text, date_commented)
-                values(?, ?, ?, ?)`, [post_id, user_id, comment_text, date_commented],
+                insert into Comments2(post_id, user_id, comment_text)
+                values(?, ?, ?)`, [post_id, user_id, comment_text],
         )
     } catch (error) {
         if (error.errno === CODE_UNIQUE_CONSTRAINT_FAILED) {
