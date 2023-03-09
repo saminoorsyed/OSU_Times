713ae5e4c7c4d2622205caa4cc25360d9b781885
Jon Riemer <86006195+Seth-9999@users.noreply.github.com>
2023-03-09 16:31:43 -0500

    Update column names (Comments)

--- api/models/usersModel.mjs
+++ api/models/usersModel.mjs
@@ -23,13 +23,15 @@ export async function getUsers() {
 // no inputs accepted
 // returns promise
 // interior data is json (array of user objects)
-export async function GetUserColumns(){
-    const [result] = await pool.query(
-        `SELECT * 
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_NAME = N'Users2';`)
-        console.log(result)
-    return result.map(({COLUMN_NAME}) => COLUMN_NAME);
+export async function getCommentColumns(){
+//     const [result] = await pool.query(`
+//                 SELECT * 
+//                 FROM INFORMATION_SCHEMA.COLUMNS 
+//                 WHERE TABLE_NAME = N'Comments2';`)
+//     return result.map(({COLUMN_NAME}) => COLUMN_NAME);
+
+    let result = ["comment_id", "post_id", "title", "user_id", "Users Full Name", "comment_text", "date_commented"]
+    return result;
 }
 
 // inputs:  (int/str, string, string, string, string)
