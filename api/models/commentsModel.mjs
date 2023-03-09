7f9215499f1d70f1bf0f7da5d6e2fc72ad33ecab
Sami Syed <syeds@flip3.engr.oregonstate.edu>
2023-03-09 14:35:07 -0800

    adjust comment model objects and column selects for ui

--- api/models/commentsModel.mjs
+++ api/models/commentsModel.mjs
@@ -17,8 +17,8 @@ export async function getCommentIDList() {
 
 export async function getComments() {
     const [result] = await pool.query(`
-    Select Comments2.comment_id, Comments2.post_id, Posts2.title, 
-        Comments2.user_id, Users2.full_name as 'Users Full Name', comment_text, date_commented
+    Select Comments2.comment_id, Posts2.title as 'post_id',
+    Users2.full_name as 'user_id', comment_text, date_commented
     from Comments2
     left join Posts2 on Comments2.post_id = Posts2.post_id
     left join Users2 on Comments2.user_id = Users2.user_id;`);
@@ -28,14 +28,14 @@ export async function getComments() {
 // no inputs accepted
 // returns promise
 // interior data is json (array of user objects)
-export async function getCommentColumns(){
-//     const [result] = await pool.query(`
-//                 SELECT * 
-//                 FROM INFORMATION_SCHEMA.COLUMNS 
-//                 WHERE TABLE_NAME = N'Comments2';`)
-//     return result.map(({COLUMN_NAME}) => COLUMN_NAME);
+export async function getCommentColumns() {
+    //     const [result] = await pool.query(`
+    //                 SELECT * 
+    //                 FROM INFORMATION_SCHEMA.COLUMNS 
+    //                 WHERE TABLE_NAME = N'Comments2';`)
+    //     return result.map(({COLUMN_NAME}) => COLUMN_NAME);
 
-    let result = ["comment_id", "post_id", "title", "user_id", "Users Full Name", "comment_text", "date_commented"]
+    let result = ["comment_id", "post_id", "user_id", "comment_text", "date_commented"]
     return result;
 }
 
@@ -65,7 +65,6 @@ export async function updateComment(comment_id, post_id, user_id, comment_text) 
 
 
     numberRecordsUpdated = result_set_header[0].affectedRows;
-    console.log(numberRecordsUpdated);
     if (numberRecordsUpdated === 1) {
         return { numUpdated: numberRecordsUpdated, status: "updated comment" };
     } else {
