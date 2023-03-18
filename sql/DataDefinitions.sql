8ebccd537d0ac1432499ea9ceb9d9f48db1da5de
saminoorsyed <saminoorsyed@gmail.com>
2023-03-18 10:39:57 -0400

    adjust sql so that it doen't have 2s

--- sql/DataDefinitions.sql
+++ sql/DataDefinitions.sql
@@ -2,41 +2,41 @@
 SET FOREIGN_KEY_CHECKS = 0;
 SET AUTOCOMMIT = 0;
 
-DROP TABLE IF EXISTS Reactions2;
-DROP TABLE IF EXISTS Comments2;
-DROP TABLE IF EXISTS Users_Authors2;
-DROP TABLE IF EXISTS Posts2;
-DROP TABLE IF EXISTS Genres2;
-DROP TABLE IF EXISTS Authors2;
-DROP TABLE IF EXISTS Users2;
-DROP TABLE IF EXISTS Administrators2;
-DROP TABLE IF EXISTS Reaction_Icons2;
+DROP TABLE IF EXISTS Reactions;
+DROP TABLE IF EXISTS Comments;
+DROP TABLE IF EXISTS Users_Authors;
+DROP TABLE IF EXISTS Posts;
+DROP TABLE IF EXISTS Genres;
+DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Administrators;
+DROP TABLE IF EXISTS Reaction_Icons;
 
-CREATE TABLE Users2
+CREATE TABLE Users
 (
     user_id             INT(11) AUTO_INCREMENT PRIMARY KEY,
     full_name           varchar(50) NOT NULL,
     username            varchar(50) NOT NULL,
     email               varchar(50)  NOT NULL,
-    CONSTRAINT          UQ_Users2_email
+    CONSTRAINT          UQ_Users_email
         UNIQUE          (email),
-    CONSTRAINT          UQ_Users2_username
+    CONSTRAINT          UQ_Users_username
         UNIQUE          (username)
 ) ENGINE=INNODB;
 
-CREATE TABLE Administrators2
+CREATE TABLE Administrators
 (
     admin_id            INT(11) AUTO_INCREMENT PRIMARY KEY,
     full_name           varchar(50) NOT NULL,
     username            varchar(50) NOT NULL,
     email               varchar(50) NOT NULL,
-    CONSTRAINT          UQ_Administrators2_username
+    CONSTRAINT          UQ_Administrators_username
         UNIQUE          (username),
-    CONSTRAINT          UQ_Administrators2_email
+    CONSTRAINT          UQ_Administrators_email
         UNIQUE          (email)
 ) ENGINE=INNODB;
 
-CREATE TABLE Authors2
+CREATE TABLE Authors
 (
     author_id           INT(11) AUTO_INCREMENT PRIMARY KEY,
     full_name           varchar(50) NOT NULL,
@@ -44,41 +44,41 @@ CREATE TABLE Authors2
     email               varchar(50) NOT NULL,
     admin_action        text,
     admin_id            INT(11),
-    CONSTRAINT          FK_Authors2_Admistrators
+    CONSTRAINT          FK_Authors_Admistrators
         FOREIGN KEY     (admin_id)
-        REFERENCES      Administrators2(admin_id)
+        REFERENCES      Administrators(admin_id)
         ON DELETE       SET NULL,
-    CONSTRAINT          UQ_Authors2_username
+    CONSTRAINT          UQ_Authors_username
         UNIQUE          (username),
-    CONSTRAINT          UQ_Authors2_email
+    CONSTRAINT          UQ_Authors_email
         UNIQUE          (email)
     
 ) ENGINE=INNODB;
 
 -- Follow table as an intersection table
-CREATE TABLE Users_Authors2
+CREATE TABLE Users_Authors
 (
     user_author_id      INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     user_id             INT(11) NOT NULL, 
     author_id           INT(11) NOT NULL,
-    CONSTRAINT          FK_Users2_Authors2_Users
+    CONSTRAINT          FK_Users_Authors_Users
         FOREIGN KEY     (user_id)
-        REFERENCES      Users2(user_id)
+        REFERENCES      Users(user_id)
         ON DELETE       CASCADE,
-    CONSTRAINT          fk_Users2_Authors2_Authors2
+    CONSTRAINT          fk_Users_Authors_Authors
         FOREIGN KEY     (author_id)
-        REFERENCES      Authors2(author_id),
-    CONSTRAINT          UQ_Users2_Authors2_user_id_author_id
+        REFERENCES      Authors(author_id),
+    CONSTRAINT          UQ_Users_Authors_user_id_author_id
         UNIQUE          (user_id, author_id)
 ) ENGINE=INNODB;
 
-CREATE TABLE Genres2
+CREATE TABLE Genres
 (
     genre_id            INT(11) AUTO_INCREMENT PRIMARY KEY,
     genre_name          varchar(50) NOT NULL
 ) ENGINE=INNODB;
 
-CREATE TABLE Posts2
+CREATE TABLE Posts
 (
     post_id             INT(11) AUTO_INCREMENT PRIMARY KEY,
     author_id           INT(11) NOT NULL,
@@ -86,42 +86,42 @@ CREATE TABLE Posts2
     title               varchar(50) NOT NULL,
     date_posted         DATETIME DEFAULT CURRENT_TIMESTAMP,
     post_text           text NOT NULL,
-    CONSTRAINT          FK_Posts2_Genres2
+    CONSTRAINT          FK_Posts_Genres
         FOREIGN KEY     (genre_id)
-        REFERENCES      Genres2(genre_id),
-    CONSTRAINT          FK_Posts2_Authors2
+        REFERENCES      Genres(genre_id),
+    CONSTRAINT          FK_Posts_Authors
         FOREIGN KEY     (author_id)
-        REFERENCES      Authors2(author_id),
+        REFERENCES      Authors(author_id),
     CONSTRAINT          UQ_Posts_title
         UNIQUE          (title)
 ) ENGINE=INNODB;
 
-CREATE TABLE Comments2
+CREATE TABLE Comments
 (
     comment_id          INT(11) AUTO_INCREMENT PRIMARY KEY,
     post_id             INT(11) NOT NULL,
     user_id             INT(11) NOT NULL,
     comment_text        text NOT NULL,
     date_commented      DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT          FK_Comments2_Users2
+    CONSTRAINT          FK_Comments_Users
         FOREIGN KEY     (user_id)
-        REFERENCES      Users2(user_id)
+        REFERENCES      Users(user_id)
         ON DELETE       CASCADE,
-    CONSTRAINT          FK_Comments2_Posts2
+    CONSTRAINT          FK_Comments_Posts
         FOREIGN KEY     (post_id)
-        REFERENCES      Posts2(post_id)
+        REFERENCES      Posts(post_id)
         ON DELETE       CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE Reaction_Icons2
+CREATE TABLE Reaction_Icons
 (
     reaction_icon_id    INT(11) AUTO_INCREMENT PRIMARY KEY,
     reaction_type       varchar(20) NOT NULL,
-    CONSTRAINT          UN_Reaction_Icons2_reaction_type
+    CONSTRAINT          UN_Reaction_Icons_reaction_type
         UNIQUE          (reaction_type)
 ) ENGINE=INNODB;
 
-CREATE TABLE Reactions2
+CREATE TABLE Reactions
 (
     reaction_id         INT(11) AUTO_INCREMENT PRIMARY KEY,
     user_id             INT(11) NOT NULL,
@@ -130,15 +130,15 @@ CREATE TABLE Reactions2
     date_reacted        DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT
         FOREIGN KEY     (user_id)
-        REFERENCES      Users2(user_id)
+        REFERENCES      Users(user_id)
         ON DELETE       CASCADE,
     CONSTRAINT
         FOREIGN KEY     (post_id)
-        REFERENCES      Posts2(post_id)
+        REFERENCES      Posts(post_id)
         ON DELETE       CASCADE,
     CONSTRAINT
         FOREIGN KEY     (reaction_icon_id)
-        REFERENCES      Reaction_Icons2(reaction_icon_id)
+        REFERENCES      Reaction_Icons(reaction_icon_id)
         ON DELETE       CASCADE 
 ) ENGINE=INNODB;
 
--- sql/SampleData.sql
+++ sql/SampleData.sql
@@ -6,96 +6,96 @@ SET AUTOCOMMIT = 0;
 
 -- Insertion Statements ********************************************************************************
 
-INSERT INTO Users2 (full_name, username, email)
+INSERT INTO Users (full_name, username, email)
     VALUES
         ("Kathryn Janeway", "Captain", "kjneway@voyager.com"),
         ("Amal Chakotay", "Commander", "achakotay@makis.com"),
         ("Tom Paris", "Lieutenant", "tparis@starfleet.com");
 
 
-INSERT INTO Administrators2 (full_name, username, email)
+INSERT INTO Administrators (full_name, username, email)
     VALUES
         ("Mister Neelix","Moral Officer", "mneelix@talax.com"),
         ("Belana Torres", "Chief Engineer", "btorees@makis.com"),
         ("Kes Ocampan", "Lead Botanist", "kocampa@telepathy.com");
 
-INSERT INTO Authors2 (full_name, username, email, admin_action, admin_id)
+INSERT INTO Authors (full_name, username, email, admin_action, admin_id)
     VALUES
         ("The Doctor", "TheDoctor", "thedoctor@hologram.com", NULL,
             NULL),
         ("Tuvok Vulcan", "Commander", "tvuclan@voyager.com", "1) on 11.3.4060 Supreme Commander expressed concern on post content 2)Warned and put on probation on 11.4.4060",
-            (SELECT admin_id FROM Administrators2 WHERE full_name = "Mister Neelix" )),
+            (SELECT admin_id FROM Administrators WHERE full_name = "Mister Neelix" )),
         ("Seven ofNine", "Borg", "sevenofnine@borg.com", "1)Promote her work to front page (Supreme commander expressed approval)",
-            (SELECT admin_id FROM Administrators2 WHERE full_name = "Belana Torres" )),
+            (SELECT admin_id FROM Administrators WHERE full_name = "Belana Torres" )),
         ("Sami Syed", "Supreme Leader", "ssyed@monarchy.com", NULL,
-            (SELECT admin_id FROM Administrators2 WHERE full_name = "Belana Torres" ));
+            (SELECT admin_id FROM Administrators WHERE full_name = "Belana Torres" ));
 
 
-INSERT INTO Users_Authors2 (user_id, author_id)
+INSERT INTO Users_Authors (user_id, author_id)
     VALUES
-        ((Select user_id from Users2 WHERE full_name = "Kathryn Janeway"),
-            (SELECT author_id FROM Authors2 WHERE full_name = "Seven ofNine")),
-        ((Select user_id from Users2 WHERE full_name = "Kathryn Janeway"),
-            (SELECT author_id FROM Authors2 WHERE full_name = "Tuvok Vulcan")),
-        ((SELECT user_id FROM Users2 WHERE full_name = "Amal Chakotay"),
-            (SELECT author_id FROM Authors2 WHERE full_name = "Tuvok Vulcan"));
+        ((Select user_id from Users WHERE full_name = "Kathryn Janeway"),
+            (SELECT author_id FROM Authors WHERE full_name = "Seven ofNine")),
+        ((Select user_id from Users WHERE full_name = "Kathryn Janeway"),
+            (SELECT author_id FROM Authors WHERE full_name = "Tuvok Vulcan")),
+        ((SELECT user_id FROM Users WHERE full_name = "Amal Chakotay"),
+            (SELECT author_id FROM Authors WHERE full_name = "Tuvok Vulcan"));
 
-INSERT INTO Genres2(genre_name)
+INSERT INTO Genres(genre_name)
     VALUES
         ("Social Science"),
         ("Engineering"),
         ("Annoucements"),
         ("Opinions");
 
-INSERT INTO Posts2 (title, post_text, author_id, genre_id)
+INSERT INTO Posts (title, post_text, author_id, genre_id)
     VALUES
         ("Ethics of the Collective", 
             "<p>In the sincerest interest of the future of humanity, I hope to shed light on the ethical system which governs the Borg collective and in many cases mirrors the human economic system known as capitalism. To start I'd like cover my own history:</p><p>My link to the Collective has been severed for nearly four years. If I die, everything that I have accomplished in that time, everything I achieved as an individual, will be lost. My memories, my experiences. It will be as if they, as if I never existed.</p>",
-            (SELECT author_id FROM Authors2 WHERE username = "Borg"),
-            (SELECT genre_id FROM Genres2 WHERE genre_name = "Social Science")),
+            (SELECT author_id FROM Authors WHERE username = "Borg"),
+            (SELECT genre_id FROM Genres WHERE genre_name = "Social Science")),
         ("An Anthropology of space farring cultures",
             "<p>My people taught me a man does not own land. He doesn't own anything but the courage and loyalty in his     heart. That's where my power comes from. This same thinking can be brought to our exploration and protection of space, whether we have a sense of ownership and stewardship over it. What do we owe the inhabitants of the spaces that we come to occupy with our ever expanding society and how do we use the resources of those spaces responsibly</p>",
-            (SELECT author_id FROM Authors2 WHERE username = "TheDoctor"),
-            (SELECT genre_id FROM Genres2 WHERE genre_name="Opinions")),
+            (SELECT author_id FROM Authors WHERE username = "TheDoctor"),
+            (SELECT genre_id FROM Genres WHERE genre_name="Opinions")),
         ("The varied uses of flux capacitors",
             "<p>The problem with flux capacitors is that they have too many uses from spacial folding to warp drive coordinate settings and the maintenance of environmental systems, flux capacitors are our friens and foes: too many plane crashes... spacial plane crashes due to excess flux energy.blah blah blah blah blah blah blah blah blah</p>",
-            (SELECT author_id FROM Authors2 WHERE username = "TheDoctor"),
-            (SELECT genre_id FROM Genres2 WHERE genre_name="Opinions"));
+            (SELECT author_id FROM Authors WHERE username = "TheDoctor"),
+            (SELECT genre_id FROM Genres WHERE genre_name="Opinions"));
 
-INSERT INTO Comments2 (comment_text, post_id, user_id)
+INSERT INTO Comments (comment_text, post_id, user_id)
     VALUES
         ("Great article Doctor!",
-            (SELECT post_id FROM Posts2 WHERE title = "An Anthropology of space farring cultures"),
-            (SELECT user_id FROM Users2 WHERE username = "Commander")),
+            (SELECT post_id FROM Posts WHERE title = "An Anthropology of space farring cultures"),
+            (SELECT user_id FROM Users WHERE username = "Commander")),
         ("Novel insight into the collective!",
-            (SELECT post_id FROM Posts2 WHERE title = "Ethics of the Collective"),
-            (SELECT user_id FROM Users2 WHERE username = "Captain")),
+            (SELECT post_id FROM Posts WHERE title = "Ethics of the Collective"),
+            (SELECT user_id FROM Users WHERE username = "Captain")),
         ("I hate the Borg!!!",
-            (SELECT post_id FROM Posts2 WHERE title = "Ethics of the Collective"),
-            (SELECT user_id FROM Users2 WHERE username = "Captain"));
+            (SELECT post_id FROM Posts WHERE title = "Ethics of the Collective"),
+            (SELECT user_id FROM Users WHERE username = "Captain"));
 
 -- image to string converter:
 -- https://codebeautify.org/image-to-base64-converter
 -- string to image converter:
 -- https://codebeautify.org/base64-to-image-converter
 
-INSERT INTO Reaction_Icons2 (reaction_type)
+INSERT INTO Reaction_Icons (reaction_type)
     VALUES
         ("like"),
         ("light bulb"),
         ("heart");
 
-INSERT INTO Reactions2 (user_id, post_id, reaction_icon_id)
+INSERT INTO Reactions (user_id, post_id, reaction_icon_id)
     VALUES
-        ((SELECT user_id FROM Users2 WHERE full_name = "Amal Chakotay"),
-            (SELECT post_id FROM Posts2 WHERE title = "An Anthropology of space farring cultures"),
-            (SELECT reaction_icon_id FROM Reaction_Icons2 WHERE reaction_type = "heart")),
-        ((SELECT user_id FROM Users2 WHERE full_name = "Amal Chakotay"),
-            (SELECT post_id FROM Posts2 WHERE title = "Ethics of the Collective"),
-            (SELECT reaction_icon_id FROM Reaction_Icons2 WHERE reaction_type = "like")),
-        ((SELECT user_id FROM Users2 WHERE full_name = "Kathryn Janeway"),
-            (SELECT post_id FROM Posts2 WHERE title = "Ethics of the Collective"),
-            (SELECT reaction_icon_id FROM Reaction_Icons2 WHERE reaction_type = "like"));
+        ((SELECT user_id FROM Users WHERE full_name = "Amal Chakotay"),
+            (SELECT post_id FROM Posts WHERE title = "An Anthropology of space farring cultures"),
+            (SELECT reaction_icon_id FROM Reaction_Icons WHERE reaction_type = "heart")),
+        ((SELECT user_id FROM Users WHERE full_name = "Amal Chakotay"),
+            (SELECT post_id FROM Posts WHERE title = "Ethics of the Collective"),
+            (SELECT reaction_icon_id FROM Reaction_Icons WHERE reaction_type = "like")),
+        ((SELECT user_id FROM Users WHERE full_name = "Kathryn Janeway"),
+            (SELECT post_id FROM Posts WHERE title = "Ethics of the Collective"),
+            (SELECT reaction_icon_id FROM Reaction_Icons WHERE reaction_type = "like"));
 
 
 SET FOREIGN_KEY_CHECKS = 1;
